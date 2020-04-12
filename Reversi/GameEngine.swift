4d4a3315ed3713a091e54823322f0d0e483aa475
史 翔新 <x_shi@yumemi.co.jp>
2020-04-12 20:48:48 +0900

    Add a class for managing game status

---
+++ Reversi/GameEngine.swift
@@ -0,0 +1,13 @@
+//
+//  GameEngine.swift
+//  Reversi
+//
+//  Created by 史 翔新 on 2020/04/12.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+
+final class GameEngine {
+    
+}
