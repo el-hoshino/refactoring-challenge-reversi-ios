56891b3049223cf691132ca8bb8db26bec2b33ae
史 翔新 <x_shi@yumemi.co.jp>
2020-04-19 06:37:28 +0900

    Fix GameEngine tests due to properties change

--- ReversiTests/GameEngineTests.swift
+++ ReversiTests/GameEngineTests.swift
@@ -26,7 +26,7 @@ class GameEngineTests: XCTestCase {
         XCTAssertEqual(engine.gameBoardHeight, 8)
         XCTAssertEqual(engine.count(of: .light), 2)
         XCTAssertEqual(engine.count(of: .dark), 2)
-        XCTAssertEqual(engine.board.standardOutput, """
+        XCTAssertEqual(engine.boardStandardOutput, """
             --------
             --------
             --------
@@ -42,11 +42,11 @@ class GameEngineTests: XCTestCase {
     
 }
 
-private extension Array where Element == Disk? {
+private extension GameEngine {
     
-    var standardOutput: String {
-        return enumerated().reduce(into: "", {
-            if $1.offset > 0 && $1.offset % GameEngine.width == 0 {
+    var boardStandardOutput: String {
+        return board.enumerated().reduce(into: "", {
+            if $1.offset > 0 && $1.offset % gameBoardWidth == 0 {
                 $0 += "\n\($1.element.symbol)"
             } else {
                 $0 += $1.element.symbol
